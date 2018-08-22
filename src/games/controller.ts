38c781ef486f74c4f7a4b481c2b7846871d50156
Jose Mesa <jose.mesa.solorzano@gmail.com>
2018-08-22 17:14:41 +0200

    last commit

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -1,4 +1,4 @@
-import { JsonController, Param , Post, HttpCode,NotFoundError, BodyParam } from 'routing-controllers'
+import { JsonController, Param , Post, HttpCode,NotFoundError, BodyParam, Get } from 'routing-controllers'
 import {Game, Player} from './entity'
 import {generateBattleshipField, shot, pcShots} from '../gameLogic.js'
 
@@ -9,10 +9,26 @@ export default class GamesController {
   @HttpCode(201)
   async createGame () {
     const game = await Game.create({status: 'started'}).save()
-    const player = await Player.create({role: 'user', game, field: generateBattleshipField()}).save()
+    await Player.create({role: 'user', game, field: generateBattleshipField()}).save()
     await Player.create({role: 'pc', game, field: generateBattleshipField()}).save()
     
-    return player.field
+    return game.id
+  }
+
+  @Get('/games/:id')
+  async getGame(
+    @Param('id') id: number
+  ) {
+    const game = await Game.findOne(id)
+    if (!game) throw new NotFoundError('Cannot find game!')
+
+    const bot = await Player.findOne({role: 'pc', game})
+    if (!bot) throw new NotFoundError('Cannot find player!')
+
+    const player = await Player.findOne({role: 'user', game})
+    if (!player) throw new NotFoundError('Cannot find player!')
+
+    return {botField: bot.field, playerField: player.field}
   }
  
   @Post('/games/:id')
@@ -43,7 +59,7 @@ export default class GamesController {
     await bot.save()
     await player.save()
 
-    return {playerHit, botField: bot.field, botHit, playerField: player.field}
+    return {playerField: player.field, playerHit, playerScore: player.score, botField: bot.field, botHit, botScore: bot.score }
 
   }
 }
